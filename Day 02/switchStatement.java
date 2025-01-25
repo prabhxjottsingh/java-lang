01d0294ddd451c7f5b1ad720729aa6ba6c908b90
Prabhjot Singh <prabhx.jott@gmail.com>
2025-01-25 21:17:12 +0530

    added new syntax for the switch statements (arrow syntax)

--- Day 02/switchStatement.java
+++ Day 02/switchStatement.java
@@ -3,6 +3,7 @@ public class switchStatement {
 
         String[] day = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
        
+        System.out.println("Before new syntax:");
         for(int i = 0; i < 7; i++){
             switch (day[i]) {
                 case "Sunday", "Friday", "Saturday":
@@ -12,6 +13,14 @@ public class switchStatement {
                 default:
                     System.out.println("Weekday");
                     break;
+            }
+        }
+
+        System.out.println("\nAfter new syntax:");
+        for(int i = 0; i < 7; i++){
+            switch (day[i]) {
+                case "Sunday", "Friday", "Saturday" -> System.out.println("Weekend");
+                default ->  System.out.println("Weekday");
             }
         }
 
